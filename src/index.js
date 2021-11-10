eef83dd46b3b629026336fd32906d68ee9d057a8
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-11-10 12:32:30 +0200

    Create a frontend part of the application to search for country data by its partial or full name.
    [JS]
    * Use the public API Rest Countries
    * Write a function fetchCountries (name) that makes an HTTP request for a resource and returns a promise with an array of countries - the result of the request
    * Sanitize the entered string with the trim () method
    * Apply Debounce on the event handler and make an HTTP request 300ms after the user has stopped typing. Use the lodash.debounce package
    * If the backend returned more than 10 countries, a notification appears in the interface that the name should be more specific. Use the notiflix library for notifications
    * If the backend returned from 2 to 10 countries, a list of found countries is displayed under the test field. Each item on the list consists of a flag and a country name
    * If there is one country, the interface displays the markup of the card with data about the country: flag, name, capital, population and languages
    * If the user entered the name of a country which does not exist, the backend will return an error with a status code of 404

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import SEARCH from './js/fetchCountries.js';
+import { Notify } from 'notiflix';
+import oneCountryTpl from './js/one-country-tpl.hbs';
+import allCountriesTpl from './js/all-countries.hbs';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('#search-box');
+const coutryListRef = document.querySelector('.country-list');
+// const countryInfoRef = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+  SEARCH.fetchCountries(inputRef.value.trim()).then(renderCoutryList).catch(onFetchError);
+}
+
+function renderCoutryList(country) {
+  if (country.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+
+  if (country.length === 1) {
+    return oneCountryMurkup(country);
+  } else {
+    return manyCountries(country);
+  }
+  //   if (country.length > 2 && country.length < 10) {
+  //     return manyCountries(country);
+  //   }
+}
+
+function oneCountryMurkup(country) {
+  //   const lang = Object.values(country[0].languages);
+  //   console.log(lang);
+
+  //   coutryListRef.innerHTML = country
+  //     .map(
+  //       country => `<li><img src="${country.flags.svg}" width = 40px alt="${country.name}">
+  //           <li>capital: ${country.capital}</li>
+  //           <li>population: ${country.population}</li>
+  //           <li>Languages: ${lang} </li>
+  //           </ul>
+  //         </li>`,
+  //     )
+  //     .join('');
+  const countryMurkup = oneCountryTpl(country);
+  coutryListRef.innerHTML = countryMurkup;
+}
+
+function manyCountries(country) {
+  coutryListRef.innerHTML = allCountriesTpl(country);
+  //   coutryListRef.innerHTML = country
+  //     .map(
+  //       country => `<li><img src="${country.flags.svg}" width = 40px alt="${country.name}">
+  //               <p><b>Name</b>: ${country.name.official}</p>
+  //             </li>`,
+  //     )
+  //     .join('');
+}
+function onFetchError(error) {
+  console.log(error);
+  Notify.failure('Oops, there is no country with that name');
+}
